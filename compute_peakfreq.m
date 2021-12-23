2450c92aada0111decc78c6cf76cef036e9f766d
matsvanes <mats.vanes@psych.ox.ac.uk>
2021-12-23 15:23:48 +0000

    small fixes.

--- compute_peakfreq.m
+++ compute_peakfreq.m
@@ -1,8 +1,8 @@
 function [pow, peakfreq] = compute_peakfreq(D, freq, taper, smoothing, doplot)
 
-if ~exist('freq','var'), freq=10; end
-if ~exist('taper','var'), taper='hanning'; end
-if ~exist('smoothing', 'var'), smoothing=5; end
+if ~exist('freq','var') || isempty(freq), freq=10; end
+if ~exist('taper','var') || isempty(taper), taper='hanning'; end
+if ~exist('smoothing', 'var') || isempty(smoothing), smoothing=5; end
 
 % get alpha power and phase time course
 d = spm2fieldtrip(D);
@@ -11,7 +11,7 @@ d.hdr.label=d.label;
 cfg         = [];
 cfg.length  = 10; % 10 second windows (0.1 Hz resolution)
 cfg.overlap = 0.5; % half overlap
-d = ft_redefinetrial(cfg, d); 
+d = ft_redefinetrial(cfg, d);
 
 
 cfg=[];
@@ -27,11 +27,11 @@ pow = ft_freqanalysis(cfg, d);
 
 sel = [1:4,11:16,21,22,25,26];
 x=mean(pow.powspctrm(sel,:));
+[val, idx] = max(x(find(pow.freq==6):find(pow.freq==14)));
+peakfreq = pow.freq(find(pow.freq==6)-1+idx);
 if doplot
   figure; plot(pow.freq, x)
   hold on
-  [val, idx] = max(x(find(pow.freq==6):find(pow.freq==14)));
-  peakfreq = pow.freq(find(pow.freq==6)-1+idx);
   vline(peakfreq)
 end
 end
