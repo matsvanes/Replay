942e9c1fc7235321f31d3f37ca077e2885dd39e6
matsvanes <mats.vanes@psych.ox.ac.uk>
2022-01-31 11:22:50 +0000

    deal with permutations

--- compute_replayTimecourse.m
+++ compute_replayTimecourse.m
@@ -1,4 +1,4 @@
-function ToRall = compute_replayTimecourse(is,iLCV,Mreplay,iRun) 
+function ToRall = compute_replayTimecourse(is,iLCV,Mreplay,iRun, iPerm)
 %% Save an inferred Replay Timecourse:
 
 %define peak sequenceness time:
@@ -6,30 +6,36 @@ function ToRall = compute_replayTimecourse(is,iLCV,Mreplay,iRun)
 for iSj=1:length(is.goodsub)
   % take that subject's CV parameter selection:
   iL = 5;% hard coded because this is used in the past. No crossvalidation!! %iLCV(iSj);
-   
+
   % load reactivation timecourse:
-  S = load([is.AnalysisPath,'classifiers/TestResting4Cell/','Rreds' num2str(iSj) '_' num2str(iRun)]); Rreds = S.Rreds;  % load this subject's preds
-  
+  if is.doPermutation==true
+    S.Rreds = apply_toRest(iSj, is, [], [], iRun, iPerm);
+  else
+    S = load([is.AnalysisPath,'classifiers/TestResting4Cell/','Rreds' num2str(iSj) '_' num2str(iRun)]); Rreds = S.Rreds;  % load this subject's preds
+  end
+
   reactivationtimecourse = S.Rreds{1,37,1,iL}; % 37 is 200 ms
   % convert to probability:
   reactivationtimecourse = 1./(1+exp(reactivationtimecourse));
-  
+
   % and logic operation for replay timecourse:
   T = [0,1,0,0,0,0,0,0;0,0,1,0,0,0,0,0;0,0,0,1,0,0,0,0;0,0,0,0,0,0,0,0; ...
     0,0,0,0,0,1,0,0;0,0,0,0,0,0,1,0;0,0,0,0,0,0,0,1;0,0,0,0,0,0,0,0];
-  
+
   ToR = zeros(size(reactivationtimecourse,1),1);
   orig = reactivationtimecourse(1:(end-2*lag),:)*T;
   proj = reactivationtimecourse((1+lag):(end-lag),:);
   ToR(1:end-2*lag,1) = nansum(orig.*proj,2);
-  
+
   ToRall(iSj,:) = ToR;
 end
-replaydir = [is.AnalysisPath,'classifiers/Sequence_by_Training_4Cell/'];
-mkdir(replaydir)
-if iRun==1
-  fname = 'STUDYII_PreplayOnset.mat';
-else
-  fname = 'STUDYII_ReplayOnset.mat';
+if ~is.doPermutation
+  replaydir = [is.AnalysisPath,'classifiers/Sequence_by_Training_4Cell/'];
+  mkdir(replaydir)
+  if iRun==1
+    fname = 'STUDYII_PreplayOnset.mat';
+  else
+    fname = 'STUDYII_ReplayOnset.mat';
+  end
+  save([replaydir,fname],'ToRall');
 end
-save([replaydir,fname],'ToRall');
