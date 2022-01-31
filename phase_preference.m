9dba6c71e830ff935ee8996f0d199a99a7150a56
matsvanes <mats.vanes@psych.ox.ac.uk>
2022-01-31 11:21:28 +0000

    adapt to valid permutation scheme

--- phase_preference.m
+++ phase_preference.m
@@ -33,7 +33,7 @@ is.nTime = 401;
 is.iRun = 2;
 is.nPerm = 1000;
 fsample=250;
-t_window=fsample/2;
+is.t_window=fsample/2;
 parc=parcellation('fmri_d100_parcellation_with_PCC_reduced_2mm_ss5mm_ds8mm.nii.gz');
 FOI = 'alpha'; % can be 'alpha', 'theta'
 
@@ -66,157 +66,169 @@ for iSj=1:length(is.goodsub)
   pow{iSj} = pow{iSj}(:,triggerpoints{iSj});
 end
 
-
-
 %% phase preference for reactivation (compared to shuffled classifier weights (over stimuli))
-% Todo: make this a function
-load([is.rootBehav,  'Exptrial.mat'])
-topphaseAllPerm = cell(length(is.goodsub), max(is.iRun), is.nPerm);
-for iSj=1:length(is.goodsub)
-  for iRun=is.iRun
-    is.doPermutation=false;
-    GoodChannel = find_goodChannel(iSj, is);
-    gnf = train_classifier(iSj,is,Exptrial, GoodChannel);
-    tmp = apply_toRest(iSj, is, gnf, GoodChannel, iRun); % get all Rreds
-    RredsAll{iSj,iRun} = tmp{1,37,1,is.lambda};
-    probAll{iSj,iRun} =  1./(1+exp(RredsAll{iSj,iRun}));
 
-    [topphase, ~] = get_percentilePhase(probAll{iSj,iRun}', phase{iSj}, t_window, is.topPercentile);
-    topphaseAll{iSj, iRun} = topphase;
-    for iParc = 1:38
-      % [~,fval(iSj, iParc)] = circ_ktest(topphase{iSj}(iParc,:), botphase{iSj}(iParc,:));
-      % compute non-uniformity of the phase data (Rayleigh test)
-      [~,ZtopPhase(:,iParc)]=circ_rtest(topphase(iParc,:));
-    end
-    ZtopPhaseAll(iSj,:) = ZtopPhase;
+for iSj = 1:length(is.goodsub)
+  is.doPermutation=false;
+  GoodChannel = find_goodChannel(iSj, is); % get good channels
+  gnf = train_classifier(iSj,is,Exptrial, GoodChannel); % get decoding models
+  iRun=2;
+  Rreds = apply_toRest(iSj, is, gnf, GoodChannel, iRun); % get all resting state decoding results
+  RredsAll{iSj} = Rreds{1,37,1,is.lambda}; % select the decoding results of the correct model
+  probAll{iSj} =  1./(1+exp(RredsAll{iSj})); % transform decoding results to probabilities
 
-    % do the same for shuffled classifier weights
-    is.doPermutation=true;
-    RredsPerm = zeros(30000,is.nStim,is.nPerm);
+  % compute phase non-uniformity of the top percentile probabilities
+  fname = [is.AnalysisPath, 'phasePreference/', sprintf('phasePrefReactivation%d_%d', iSj, iRun)];
+  [topphase{iSj}, ZtopPhase(iSj,:)] = phase_nonuniformity(probAll{iSj}, phase{iSj}, is, fname);
+
+
+  % do the same for permutations, where channel weights from the decoding
+  % model are permuted over stimuli
+  is.doPermutation=true;
+  fname = [fname, '_perm'];
+  if is.usePrecomputed && isfile([fname, '.mat'])
+    tmp=load(fname);
+    topphasePermAll{iSj} = tmp.topphasePerm;
+    ZtopPhasePermAll(iSj,:,:) = tmp.ZtopPhasePerm;
+  else
     for iPerm=1:is.nPerm
       gnfperm=permute_classifierweights(gnf,is);
-      tmp = apply_toRest(iSj, is, gnfperm, GoodChannel, iRun, iPerm); % get all Rreds
-      probPerm = 1./(1+exp(tmp{1,37,1,is.lambda}));
-      [topphasePerm{iPerm}, ~] = get_percentilePhase(probPerm', phase{iSj}, t_window, is.topPercentile);
-      for iParc = 1:38
-        % [~,fval(iSj, iParc)] = circ_ktest(topphase{iSj}(iParc,:), botphase{iSj}(iParc,:));
-        % compute non-uniformity of the phase data (Rayleigh test)
-        [~,ZtopPhasePerm(iParc,iPerm)]=circ_rtest(topphasePerm{iPerm}(iParc,:));
-      end
+      Rreds = apply_toRest(iSj, is, gnfperm, GoodChannel, iRun, iPerm); % get all Rreds
+      probPerm = 1./(1+exp(Rreds{1,37,1,is.lambda}));
+      [topphasePerm{iPerm}, ZtopPhasePerm(:,iPerm)] = phase_nonuniformity(probPerm, phase{iSj}, is, []);
     end
-    ZtopPhasePermAll(iSj,:,:) = ZtopPhasePerm;  
-    fname = [is.AnalysisPath, 'phasePreference/', sprintf('phasePrefReactivation%d_%d', iSj, iRun)];
-    save(fname, 'topphase', 'ZtopPhase', 'topphasePerm', 'ZtopPhasePerm')
+    save(fname, 'topphasePerm', 'ZtopPhasePerm')
+    topphasePermAll{iSj} = topphasePerm;
+    ZtopPhasePermAll(iSj,:,:) = ZtopPhasePerm;
   end
 end
 
-GA_mean = mean(ZtopPhaseAll);
+ZtopPhasePerm = mean(ZtopPhasePermAll,3);
 
-% do a statistical test | TODO: is this valid?
-p_val = sum(ZtopPhasePermAll>ZtopPhaseAll,3)/is.nPerm; % first level t test
+phasepref_reactivation = ZtopPhase-ZtopPhasePerm;
+GA_phasepref_reactivation = mean(phasepref_reactivation);
+
+% Do signflipping test on the group level
+dat=[];
+dat.label=parc.labels;
+dat.time=0;
+dat.dimord = 'rpt_chan_time';
+dat.avg = ZtopPhase;
+
+datperm=dat;
+datperm.avg = ZtopPhasePerm;
+
+Nsub=length(is.goodsub);
+cfgs=[];
+cfgs.method = 'montecarlo';
+cfgs.statistic = 'depsamplesT';
+cfgs.design(1,1:2*Nsub)  = [ones(1,Nsub) 2*ones(1,Nsub)];
+cfgs.design(2,1:2*Nsub)  = [1:Nsub 1:Nsub];
+cfgs.correctm = 'bonferroni';
+cfgs.tail = 1;
+cfgs.ivar = 1;
+cfgs.uvar = 2;
+cfgs.numrandomization=10000;
+stat=ft_timelockstatistics(cfgs,dat,datperm);
+
+% Visualize mean difference
 figure
 filename = [savebase 'phasepreference_reactivation_', sprintf('%s', FOI)];
-parc.savenii(GA_mean, filename);
+parc.savenii(GA_phasepref_reactivation, filename);
 osl_render4D([filename, '.nii.gz'], 'visualise', true);
 
-f1=figure('WindowState','maximized');
-plot_phaseConcentration(topphase, botphase, z_top, z_bot, FOI,'reactivation')
-saveas(f1,[filename, '.png'])
-% stat = stat_phasePreference(z_top, z_bot);
+filename = [savebase 'phasepreference_reactivation_', sprintf('%s', FOI), '_tstat'];
+parc.savenii(stat.stat, filename);
+osl_render4D([filename, '.nii.gz'], 'visualise', true);
 
-%% phase preference for replay (top vs bottom percentile)
-clear z_top z_bot botphase topphase GA_diff
+%% phase preference for replay (top vs permutation)
+% Do the same as above, but now for replay
+
+% observed data
 if whichstudy==1
-  load([wd,'GenericReplayData/STUDYI_ReplayOnset/RwdReplay2ndOnset'],'ToRall');
-  replayScores = ToRall;
+  tmp=load([wd,'GenericReplayData/STUDYI_ReplayOnset/RwdReplay2ndOnset'],'ToRall');
+  replayScores = tmp.ToRall;
 else
-  load([is.AnalysisPath,'classifiers/Sequence_by_Training_4Cell/', 'STUDYII_ReplayOnset'],'ToRall');
-  replayScores = ToRall;
+  tmp=load([is.AnalysisPath,'classifiers/Sequence_by_Training_4Cell/', 'STUDYII_ReplayOnset'],'ToRall');
+  replayScores = tmp.ToRall;
 end
 
+replayScoresPerm = zeros(22,30000,is.nPerm);
 for iSj=1:length(is.goodsub)
-  % take the phase of these points
-  [topphase{iSj}, botphase{iSj}] = get_percentilePhase(replayScores(iSj,:), phase{iSj}, t_window, is.topPercentile);
+  is.doPermutation=false;
+  % compute phase non-uniformity of the top percentile Replay scores
+  fname = [is.AnalysisPath, 'phasePreference/', sprintf('phasePrefReplay%d_%d', iSj, iRun)];
+  [topphase{iSj}, ZtopPhase(iSj,:)] = phase_nonuniformity(replayScores(iSj,:)', phase{iSj}, is, fname);
+end
 
-  for iParc = 1:38
-    % [~,fval(iSj, iParc)] = circ_ktest(topphase{iSj}(iParc,:), botphase{iSj}(iParc,:));
-    % compute non-uniformity of the phase data (Rayleigh test)
-    [~,z_top(iSj,iParc)]=circ_rtest(topphase{iSj}(iParc,:));
-    [~,z_bot(iSj,iParc)]=circ_rtest(botphase{iSj}(iParc,:));
+% do the same for permutations, where channel weights from the decoding
+% model are permuted over stimuli
+is.doPermutation=true;
+if is.usePrecomputed && isfile([fname, '.mat'])
+  tmp=load(fname);
+  topphasePermAll{iSj} = tmp.topphasePerm;
+  ZtopPhasePermAll(iSj,:,:) = tmp.ZtopPhasePerm;
+else
+  % We need to use the lag that is used in the observed statistic. In
+  % order to find it, we use Mreplay (group mean replay) to feed into
+  % compute_sequenceness
+  StimAll = load([is.AnalysisPath, 'classifiers/Sequence_by_Training_4Cell/StimAll.mat']);
+  sfAll = StimAll.sfAll;
+  sbAll = StimAll.sbAll;
+  [~, ~, Mreplay, ~] = find_bestLambda(is, sfAll, sbAll);
+  is.plot=false;
+  for iPerm=1:is.nPerm
+    % The permuted Rreds should already have been computed, so no need to
+    % do the stuff that comes before. This is done for all subjects at once
+    % though, and we want to save the permutation statistic per subject -
+    % so that's why we use a seperate loop.
+    replayScoresPerm(:,:,iPerm) = compute_replayTimecourse(is,[],Mreplay,iRun,iPerm);
+  end
+  for iSj=1:length(is.goodsub)
+    for iPerm=1:is.nPerm
+      [topphasePerm{iPerm}, ZtopPhasePerm(:,iPerm)] = phase_nonuniformity(squeeze(replayScoresPerm(iSj,:,iPerm))', phase{iSj}, is, []);
+    end
+    fname = [is.AnalysisPath, 'phasePreference/', sprintf('phasePrefReplay%d_%d', iSj, iRun)];
+    fname = [fname, '_perm'];
+    topphasePermAll{iSj} = topphasePerm;
+    ZtopPhasePermAll(iSj,:,:) = ZtopPhasePerm;
+    save(fname, 'topphasePerm', 'ZtopPhasePerm')
   end
 end
-GA_diff = mean(z_top-z_bot);
+
+ZtopPhasePerm = mean(ZtopPhasePermAll,3);
+
+phasepref_replay = ZtopPhase-ZtopPhasePerm;
+GA_phasepref_replay = mean(phasepref_replay);
+
+% Do signflipping test on the group level
+dat=[];
+dat.label=parc.labels;
+dat.time=0;
+dat.dimord = 'rpt_chan_time';
+dat.avg = ZtopPhase;
+
+datperm=dat;
+datperm.avg = ZtopPhasePerm;
+
+Nsub=length(is.goodsub);
+cfgs=[];
+cfgs.method = 'montecarlo';
+cfgs.statistic = 'depsamplesT';
+cfgs.design(1,1:2*Nsub)  = [ones(1,Nsub) 2*ones(1,Nsub)];
+cfgs.design(2,1:2*Nsub)  = [1:Nsub 1:Nsub];
+cfgs.correctm = 'bonferroni';
+cfgs.tail = 1;
+cfgs.ivar = 1;
+cfgs.uvar = 2;
+cfgs.numrandomization=10000;
+stat=ft_timelockstatistics(cfgs,dat,datperm);
+
+% Visualize mean difference
 filename = [savebase 'phasepreference_replay_', sprintf('%s', FOI)];
-% parc.savenii(GA_diff, filename);
-% osl_render4D([filename, '.nii.gz'], 'visualise', true);
+parc.savenii(GA_phasepref_replay, filename);
+osl_render4D([filename, '.nii.gz'], 'visualise', true);
 
-f2=figure('WindowState','maximized');
-plot_phaseConcentration(topphase, botphase, z_top, z_bot, FOI,'replay')
-saveas(f2,[filename, '.png'])
-%% Legacy
-%{
-    for iC=1:nStim
-      % use regression models
-      Rreds{iRun}(:,iC) = -(data * gnf{iC}.beta + gnf{iC}.Intercept);   % work on the X*beta space! MVE: the minus is because of a weird matlab definition. Now high positive values mean high probability
-    end
-    prob{iRun} = 1./(1+exp(Rreds{iRun})); % the Rreds are the negative of how they usually are defined. That's why we're using +Rreds instead of minus (which is normally in the logsigmoid function)
-    
-    % find highest percentile probabilities
-    x=prob{iRun}; x(isnan(x))=-Inf;
-    [val, idx] = sort(prob{iRun}, 'descend', 'MissingPlacement', 'last');
-    n = round((size(val,1)-sum(isnan(data(:,1))))*perc/100);
-    idx_top = idx(1:n,:);
-    val_top{iRun} = val(1:n,:);
-
-    
-    
-    % find lowest percentile probabilities
-    [val, idx] = sort(prob{iRun}, 'ascend', 'MissingPlacement', 'last');
-    n = round((size(val,1)-sum(isnan(data(:,1))))*perc/100);
-    idx_bottom = idx(1:n,:);
-    val_bottom{iRun} = val(1:n,:);
-    
-    % Get alpha phase from parcel time courses.
-    S=[];
-    S.outfile = fullfile(dir_temp, 'parcelphase');
-    S.D = RST;
-    RST_copy = spm_eeg_copy(S);
-    [phase, pow, maxidx] = getphasetimecourse(RST_copy);
-    phase = phase(maxidx, :); % take the parcel with maximum power
-    % TODO: Continue here!
-    time = 0:1/RST.fsample:(size(data,1)-1)/RST.fsample;
-    f=10;
-    clear i
-    a = cos(2*pi*f*time) + i*sin(2*pi*f*time); % raw alpha signal
-    phase = angle(a);
-    phase = repmat(phase, [8 1])';
-    for k=1:8
-      % take the phases of the highest/lowest probabilities
-      phase_top{iRun}(:,k) = phase(idx_top(:,k));
-      phase_bottom{iRun}(:,k) = phase(idx_bottom(:,k));
-      % probably we can use the parametric test, but this assumes max 1
-      % mode, and a "gaussian" distribution around it.
-      % alternatives are the nonparametric Hodges-Ajne test (circ_otest),
-      % but this doesn't return a test statistic. Other options is to
-      % compare the distribution with a uniform distribution using the
-      % Kuiper test (circ_kuipertest) or Winson's U2 test.
-      [pval(iRun,k), z(iRun,k)] = circ_rtest(phase_top{iRun}(:,k));
-      %       [pval(iRun,k), m(iRun,k)] = circ_otest(phase_top{iRun}(:,k));
-      subplot(2,8,(iRun-1)*8+k);
-      rose(phase_top{iRun}(:,k)); hold on
-      rose(phase_bottom{iRun}(:,k));
-      if (iRun-1)*8+k == 4
-        title('pre-task Resting State')
-      elseif (iRun-1)*8+k == 12
-        title('post-task Resting State')
-      end
-    end
-    for k=1:8
-      [~,fval(iRun,k)] = circ_ktest(phase_top{iRun}(:,k), phase_bottom{iRun}(:,k));
-    end
-  end
-  % optionally - use Watson's U2 statistic to compare the two circular
-  % distributions. See https://uk.mathworks.com/matlabcentral/fileexchange/43543-pierremegevand-watsons_u2
-  % or possibly use circ_ktest for equal concentration parameter?
-end
-%}
+filename = [savebase 'phasepreference_replay_', sprintf('%s', FOI), '_tstat'];
+parc.savenii(stat.stat, filename);
+osl_render4D([filename, '.nii.gz'], 'visualise', true);
