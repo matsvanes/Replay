137de7c243cb89354179723f3f2114a8cbdece1f
matsvanes <mats.vanes@psych.ox.ac.uk>
2022-01-31 11:24:53 +0000

    only get top percentiles in most cases - bottom percentiles are optionable

--- get_percentilePhase.m
+++ get_percentilePhase.m
@@ -1,11 +1,12 @@
 function [topphase, botphase] = get_percentilePhase(prob, phase, t_window, percentile)
 
-  % get top percentile probabilities
-  topperc = prob > prctile(prob(:),100-percentile);
-  topperc = [zeros(size(topperc,1),1),diff(topperc,[],2)]==1;% eliminate adjacent points
-  topperc(:,1:t_window)=0;topperc(:,end-t_window:end)=0;% eliminate border points:
-  topidx = upsample_replayTimes(topperc, phase, t_window);
+% get top percentile probabilities
+topperc = prob > prctile(prob(:),100-percentile);
+topperc = [zeros(size(topperc,1),1),diff(topperc,[],2)]==1;% eliminate adjacent points
+topperc(:,1:t_window)=0;topperc(:,end-t_window:end)=0;% eliminate border points:
+topidx = upsample_replayTimes(topperc, phase, t_window);
 
+if nargout>1
   % get bottom percentile probabilities
   botperc = prob < prctile(prob(:),percentile);
   botperc = [zeros(size(topperc,1),1),diff(botperc,[],2)]==1;% eliminate adjacent points
@@ -19,7 +20,8 @@ function [topphase, botphase] = get_percentilePhase(prob, phase, t_window, perce
   ix2=randperm(numel(botidx));
   topidx = topidx(ix1(1:n));
   botidx = botidx(ix2(1:n));
-
+  
   % take the phase of these points
-  topphase = phase(:,topidx);
   botphase = phase(:,botidx);
+end
+topphase = phase(:,topidx);
