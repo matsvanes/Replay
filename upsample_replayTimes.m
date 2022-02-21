184572fd9a9e626e3263b9d5b980f5ae960b1efc
matsvanes <mats.vanes@psych.ox.ac.uk>
2022-02-21 11:12:29 +0000

    adapt to additional frequency dimension

--- upsample_replayTimes.m
+++ upsample_replayTimes.m
@@ -3,14 +3,14 @@ function upsampled = upsample_replayTimes(replaytimes, phase, t_window)
 % upsample for state timecourses at higher sampling rate than replay
 % scores. 
 [~,original] = find(replaytimes);
-if size(phase,2)>length(replaytimes)
-  Q = size(phase,2) / length(replaytimes);
+if size(phase,3)>length(replaytimes)
+  Q = size(phase,3) / length(replaytimes);
   upsampled = round(original*Q);
 else
   Q = 1;
   upsampled = original;
 end
-if any(upsampled>size(phase,2)-t_window)
-  upsampled(upsampled>size(phase,2)-t_window) = [];
-  original(original>size(phase,2)-t_window) = [];
+if any(upsampled>size(phase,3)-t_window)
+  upsampled(upsampled>size(phase,3)-t_window) = [];
+  original(original>size(phase,3)-t_window) = [];
 end
