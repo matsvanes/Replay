980f0b474e6aea0d211ebf1ba7fb4ac951daa0f7
Mats Van Es (transfer) <psyc1435_transfer@OPJMBPr15-17.local>
2022-06-01 14:27:26 +0100

    remove unused

--- scaleFunc.m
+++
@@ -1,9 +0,0 @@
-function [newX] = scaleFunc(X)
-
-% scaleFact = 0.6*max(max(X(:)),-min(X(:)));  % another way to do it
-% scaleFact = max(abs(X(:)));
-scaleFact = prctile(abs(X(:)),95);
-newX = X ./ scaleFact;
-
-
-end
